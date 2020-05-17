82e08ce023e087b57e5fa4c876e38fe09f54043e
Ramenm <rmatveev21@gmail.com>
2020-05-17 14:49:01 +0300

    add gitignore and some changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 import string
+import selenium
 from selenium.webdriver import Chrome
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -25,8 +26,8 @@ class Yandex():
         # may include verification of a phone number or email
         try:
             wait_for_page_load = WebDriverWait(webdriver, 4).until(EC.url_contains("registration/mail"))
-        except:
-            raise errors.BadIpRequestException
+        except selenium.common.exceptions.TimeoutException:
+            raise errors.BadIpRequestException('Try to change proxy or wait')
 
         # webdriver.find_element_by_xpath('').send_keys('')
         webdriver.find_element_by_xpath('//*[@id="firstname"]').send_keys(self.firstname)
@@ -40,19 +41,22 @@ class Yandex():
         # solve the captcha
         try:
             wait_for_element = WebDriverWait(webdriver, 300).until(EC.url_contains("avatar"))
-        except:
-            raise errors.CaptchaFailedException
-        webdriver.find_element_by_xpath('//*[@id="root"]/div/div[1]/div[2]/main/div/div/div[3]/a').click()
-    def check_last_msg(self, webdriver):
+        except selenium.common.exceptions.TimeoutException:
+            raise errors.CaptchaFailedException('solve the captcha')
+        print('')
+    def open_last_msg(self, webdriver):
         webdriver.get('https://mail.yandex.ru/')
         first_message = webdriver.find_elements_by_class_name('js-messages-item-checkbox-controller')[0]
         print(first_message)
         print(dir(first_message))
         first_message.click()
+    def _take_first_code(self, webdriver):
         code = webdriver.find_element_by_xpath(
             '//*[@id="nb-1"]/body/div[2]/div[5]/div/div[3]/div[3]/div[2]/div[5]/div[1]/div/div[3]/div/div/table/tbody/tr/td/center/table[2]/tbody/tr/td/table/tbody/tr/td/table/tbody/tr[3]/td/div')
         print(code)
         print(dir(code))
+    def _take_2fa_code(self, webdriver):
+        pass
 
 
 
@@ -70,7 +74,11 @@ class Epicgamesstore():
         webdriver.find_element_by_xpath('//*[@id="termsOfService"]').click()
         webdriver.find_element_by_xpath('//*[@id="btn-submit"]').click()
 
-
+class Farmer():
+    def __init__(self, cls):
+        self.__dict__ = cls.__dict__
+    def run(self):
+        pass
 
 
 if __name__ == '__main__':
@@ -79,4 +87,4 @@ if __name__ == '__main__':
     epic = Epicgamesstore(a)
     yan.register(a.webdriver)
     epic.register(a.webdriver)
-    yan.check_last_msg()
+    yan.open_last_msg(a.webdriver)
